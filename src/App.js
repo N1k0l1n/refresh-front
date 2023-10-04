fdcbfed32acb65ccf2fc952fc5980194d3f3b526
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-10-04 16:13:00 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./pages/Public";
+import Login from "./pages/Login";
+import Welcome from "./pages/Welcome";
+import RequireAuth from "./RequireAuth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+
+        {/* public routes */}
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
+
+        {/* protected routes */}
+        <Route element={<RequireAuth />} />
+        <Route path="welcome" element={<Welcome />} />
+
+      </Route>
+    </Routes>
   );
 }
 
